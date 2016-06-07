74e10e7c0bf49affd8521e142b7bb22a59ea3e2e
libertinistic <libertinistic@gmail.com>
2016-06-07 14:44:26 -0700

    Revert "Pair dev"

--- pairBlogApp/starter-code/scripts/article.js
+++ pairBlogApp/starter-code/scripts/article.js
@@ -1,14 +1,9 @@
 var articles = [];
 
 function Article (opts) {
-  this.title = opts.title;
-  this.category = opts.category;
+  // TODO: Use the js object passed in to complete this contructor function:
+  // Save ALL the properties of `opts` into `this`.
   this.author = opts.author;
-  this.authorUrl = opts.authorUrl;
-  this.publishedOn = opts.publishedOn;
-  this.body = opts.body;
-
-  return this;
 }
 
 Article.prototype.toHtml = function() {
@@ -16,20 +11,20 @@ Article.prototype.toHtml = function() {
 
   $newArticle.attr('data-category', this.category);
 
-  $('.template h1').text(this.title);
-  $('.template a').text(this.author);
-  $('.template a').attr('href',this.authorUrl);
-  $('.template .article-body').html(this.body);
+  // TODO: Use jQuery to fill in the template with properties
+  // from this particular Article instance. We need to fill in:
+  // the author name and url, the article title and body, and the
+  // publication date.
 
   // Include the publication date as a 'title' attribute to show on hover:
-  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn)
 
   // Display the date as a relative number of "days ago":
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
 
-  $('.template').append('<hr>');
+  $newArticle.append('<hr>');
 
-  $('article').removeClass('template');
+  // TODO: This cloned article is no longer a template, so we should remove that class...
 
   return $newArticle;
 }
@@ -40,7 +35,7 @@ rawData.sort(function(a,b) {
 
 rawData.forEach(function(ele) {
   articles.push(new Article(ele));
-});
+})
 
 articles.forEach(function(a){
   $('#articles').append(a.toHtml())
