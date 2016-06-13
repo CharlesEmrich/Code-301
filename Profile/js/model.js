bd2a114b7e22add1b43ea597d634d0bfcd271d4c
libertinistic <libertinistic@gmail.com>
2016-06-12 22:57:09 -0700

    began work on Handlebars, fixed tabbed browsing

---
+++ Profile/js/model.js
@@ -0,0 +1,31 @@
+//Build Project Sections
+var projects = [];
+var template = $('#project-template');
+var compiledTemplate = Handlebars.compile(template);
+// var template = Handlebars.compile($('#project-template').html()); //Can I compress the first two steps of Handlebars like this?
+
+function Project (keys) {
+  this.name = keys.name;
+  this.date = new Date(keys.date);
+  this.img  = keys.img;
+  this.desc = keys.desc;
+  this.repo = keys.repo;
+}
+
+Project.prototype.toHTML = function() {
+  return this.compiledTemplate();
+};
+
+if (typeof rawProjects !== 'undefined') {
+  rawProjects.sort(function(a,b) {
+    return (new Date(b.date)) - (new Date(a.date));
+  });
+
+  rawProjects.forEach(function(ele) {
+    projects.push(new Project(ele));
+  });
+}
+
+projects.forEach(function(a) {
+  $('#portfolio').append(a.toHTML());
+});
--- Profile/js/view.js
+++ Profile/js/view.js
@@ -1,53 +1,14 @@
-//rendering project section
-var projects = [];
-
-function Project (keys) {
-  this.name = keys.name;
-  this.date = new Date(keys.date);
-  this.img  = keys.img;
-  this.desc = keys.desc;
-}
-
-Project.prototype.toHTML = function() {
-  var $newProject = $('article.template').clone();
-
-  $newProject.find('.folioName').text(this.name);
-  $newProject.find('.folioPic').attr('src',this.img);
-  $newProject.find('.folioDesc').text(this.desc);
-  $newProject.find('.date').text(this.date);
-  //Implement projectUrl here.
-
-    // // Include the publication date as a 'title' attribute to show on hover:
-    // $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-    //
-    // // Display the date as a relative number of "days ago":
-    // $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago')
-  $('.article').removeClass('template');
-  return $newProject;
-};
-
-rawProjects.sort(function(a,b) {
-  return a.date - b.date; //double-check this
-});
-
-rawProjects.forEach(function(idx) {
-  projects.push(new Project(idx));
-});
-
-//Should everything above this point be in control? Or even model? Inquire.
-
 projects.forEach(function(a) {
   $('#portfolio').append(a.toHTML());
 });
 
 var initView = function() { //Initialize view state and set event listeners.
   var tabContent = $('.tab-content'); //cache jQ
-  // tabContent.hide(); //Hide all tabs
+  tabContent.hide(); //Hide all tabs
   $('.main-nav').on('click', '.tab', function(e) { //What function does .tab in the data param do here? I have no idea why it makes this work.
-    // tabContent.hide();
+    tabContent.hide();
     console.log('#' + $(this).data('content'));
     $('#' + $(this).data('content')).fadeIn();
-    //TODO: Determine why the preceding line doesn't work for the portfolio tab.
   });
 };
 
