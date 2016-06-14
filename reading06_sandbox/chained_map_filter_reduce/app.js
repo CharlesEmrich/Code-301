51d53a962f4b588fb61d4fc639f25c71528c42ed
libertinistic <libertinistic@gmail.com>
2016-06-13 21:59:13 -0700

    finished EC chaining sandbox

--- reading06_sandbox/chained_map_filter_reduce/app.js
+++ reading06_sandbox/chained_map_filter_reduce/app.js
@@ -18,4 +18,13 @@ $(function() {
   }
   var reduceX = x.reduce(chew);
   console.log('the product of the whole of x is ' + reduceX);
+
+  chainedX = x.filter(odd).map(square);
+  console.log('the squares of all the odd numbers in x are ' + chainedX);
+
+  function summit(a,b) {
+    return a + b;
+  }
+  ultimateChainedX = x.filter(odd).map(square).reduce(summit);
+  console.log('...and the sum of all those squares of odd numbers is: ' + ultimateChainedX);
 });
