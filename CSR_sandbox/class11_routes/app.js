4b7768ef14d44edb8c7433e34af178683ea13857
libertinistic <libertinistic@gmail.com>
2016-06-20 12:08:18 -0700

    added getJSON call, template parsing, and DOM appending of articles

--- CSR_sandbox/class11_routes/app.js
+++ CSR_sandbox/class11_routes/app.js
@@ -12,6 +12,7 @@ function setRouteMappings() {
 
   page();
 }
+
 function articles() { //Show articles
 }
 function index() { p.text('viewing index (home)'); }
@@ -25,5 +26,19 @@ function notfound() {
               + '" can\'t be found (like a bridge over troubled water, ya\' know)!</p>'
               + '<img src="lost.jpg"></img>';
 }
+function articulate(a) {
+  var template = $('#article-template').html();
+  var compTemplate = Handlebars.compile(template);
+  a.map(function(ele) {
+    // console.log(ele);
+    var newArt = compTemplate(ele);
+    $('.articles').append(newArt);
+    // console.log(compTemplate(a));
+  });
+}
 
+$.getJSON('ipsumArticles.json', articulate)
+  .always(function() {
+    console.log('getJSON Called.');
+  });
 setRouteMappings();
